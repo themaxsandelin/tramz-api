092fba6a294678b3ff9bf5dda74d07ebc3758400
Max Sandelin <themaxsandelin@gmail.com>
2017-06-20 14:38:40 +0200

    Init.

---
+++ README.md
@@ -0,0 +1,5 @@
+# tramz-api
+The public API for the Tramz CLI.
+
+## LICENSE
+[MIT](LICENSE) © [Max Sandelin](https://maxsandelin.com/)
---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const request = require('request');
+const dotenv = require('dotenv').config();
+
+const api = express();
+
+api.get('/', (req, res, next) => {
+  res.send('Hello! 👋🏻');
+});
+
+api.get('/token', (req, res, next) => {
+  const key = process.env.KEY;
+  const secret = process.env.SECRET;
+
+  request.post('https://api.vasttrafik.se:443/token', {
+    headers: {
+      'Authorization': 'Basic ' + new Buffer(key + ':' + secret).toString('base64')
+    },
+    body: 'grant_type=client_credentials'
+  }, (error, response, body) => {
+    if (error) reject(error);
+
+    res.set('Content-Type', 'application/json');
+    res.send(body);
+  });
+});
+
+api.listen(80, () => {
+  console.log('API up and running. 🎉');
+});
