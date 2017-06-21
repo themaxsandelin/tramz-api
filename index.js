cab344a257f1a3d51c9461a193335ea3adc61154
Max Sandelin <themaxsandelin@gmail.com>
2017-06-21 15:04:43 +0200

    Added simple logging middleware and handle 404

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
+const fs = require('fs');
+const moment = require('moment');
 const express = require('express');
 const request = require('request');
 const dotenv = require('dotenv').config();
+const useragent = require('express-useragent');
 
 const api = express();
 
-api.get('/', (req, res, next) => {
+api.use(useragent.express());
+api.use((req, res, next) => {
+  const now = moment();
+  const ua = {
+    ip: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
+    path: req.originalUrl,
+    platform: req.useragent.platform,
+    os: req.useragent.os,
+    timestamp: now.format('x'),
+    browser: {
+      name: req.useragent.browser,
+      version: req.useragent.version
+    }
+  };
+
+  if (!fs.existsSync('./log')) fs.mkdirSync('./log');
+  const logPath = './log/' + now.format('YYYY-MM-DD') + '.json';
+
+  const data = (fs.existsSync(logPath)) ? JSON.parse(fs.readFileSync(logPath)):[];
+
+  data.push(ua);
+  fs.writeFileSync(logPath, JSON.stringify(data));
+  next();
+});
+
+api.get('/', (req, res) => {
   res.send('Hello! 👋🏻');
 });
 
-api.get('/token', (req, res, next) => {
+api.get('/token', (req, res) => {
   const key = process.env.KEY;
   const secret = process.env.SECRET;
 
@@ -24,6 +52,10 @@ api.get('/token', (req, res, next) => {
     res.set('Content-Type', 'application/json');
     res.send(JSON.stringify({ token: token }));
   });
+});
+
+api.get('*', (req, res) => {
+  res.status(404).send('wat?');
 });
 
 api.listen(3000, () => {
